b9b4366442e65a32b4a981093202f7a5433f3cb2
ANSHAD134 <anshadck1020@gmail.com>
2025-08-15 19:55:48 +0530

    Add files via upload

---
+++ C Programming Files/First C Program/First Program.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("Anshad ck\n");
+    printf("Address: Cherukatt house\n");
+    printf("phone number: 7736090873\n");
+    printf("Email Id: anshadck1020@gmail.com\n");
+    return 0;
+}
